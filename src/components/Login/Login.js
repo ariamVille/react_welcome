0d37ef8bde215a7bc9065bf7b23a87791b3e47e8
ariamVille <mariasoulevillanueva@gmail.com>
2018-04-08 01:50:43 +0800

    added functions to components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import './Login';
 class Login extends Component {
+
+  constructor(props){
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.login = this.login.bind(this);
+    this.onChange = this.onChange.bind(this);
+
+  }
+
+  login(){
+    console.log("login function");
+  }
+
+  onChange(e){
+    this.setState({[e.target.name]: e.target.value});
+    console.log(this.state);    
+  }
+
   render() {
     return (
-      <div className="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4">
-        <div className="cell">
+      <div className="row small-up-2 medium-up-3 large-up-4">
+        <div className="column bodyPart" >
           <h2>Login page</h2>
+          <label>Username</label>
+          <input type="text" name="username" placeholder="input username" onChange={this.onChange}/>
+          <label>Password</label>
+          <input type="password" name="password" placeholder="input password" onChange={this.onChange}/>
+          <input type="submit" value="Login" className="button" onClick={this.login}/>
         </div>
       </div>
     );
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -3,9 +3,11 @@ import './Welcome';
 class Welcome extends Component {
   render() {
     return (
-      <div className="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4">
-        <div className="cell">
-          <h2>Welcome!</h2>
+      <div className="row small-up-2 medium-up-3 large-up-4">
+        <div className="cell ">
+          <h2>Project Description</h2>
+          <a href='/login' class="button ">Login</a>
+          <a href='/Signup' class="button success">Signup</a>
         </div>
       </div>
     );
