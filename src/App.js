2d9ddd23bf7e06a22d32d287447f193aa1aa4a0f
ariamVille <mariasoulevillanueva@gmail.com>
2018-04-08 00:40:05 +0800

    added components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import logo from './logo.svg';
 import './styles/foundation.min.css';
 import './styles/custom.css';
 import Header from './components/Header/Header';
+import Routes from './routes';
 import MobileHeader from './components/MobileHeader/MobileHeader';
 import Footer from './components/Footer/Footer';
-import Welcome from './components/Welcome/Welcome';
 
 class App extends Component {
 
@@ -19,24 +19,17 @@ class App extends Component {
 
   render() {
     return (
-
       <div className="off-canvas-wrapper">
-
         <div className="off-canvas-inner" data-off-canvas-wrapper>
-
             <div className="off-canvas-content" data-off-canvas-content>
-                  
                  <MobileHeader name={this.state.appName}/>
                  <Header name={this.state.appName}/>
-                 <Welcome/>
+                 <Routes name={this.state.appName}/>
                   <hr/>
                  <Footer/>
             </div>
-          
         </div>
-    
     </div>
-
     );
   }
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './NotFound';
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4">
+        <div className="cell">
+          <h2>404 no found!</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
